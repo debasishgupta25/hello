5d452afc7adbae30720fb5a0234a2413bc1bf61f
Debasish Gupta <debasish.gupta25@gmail.com>
2025-04-30 21:58:41 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
 import CountryNode from './CountryNode';
 import exampleJson from "./../example/example.json";
-
-const countryData = exampleJson[0];
+import {useState, useEffect} from "react";
+import TreeNode from "./model/TreeNode";
 
 const App = () => {
+  const [searchText, setSearchText] = useState("");
+  const [countryData, setCountryData] = useState<TreeNode[]>(exampleJson);
+
+  const filterDataTree = (nodes: TreeNode[], searchTerm: string): TreeNode[] => {
+      return nodes
+        .map((node) => {
+          if (node.children) {
+            node.children = filterDataTree(node.children, searchTerm?.toLowerCase());
+          }
+          return node;
+        })
+        .filter((node) => node.name?.toLowerCase().includes(searchTerm?.toLowerCase()) || (node.children && node.children.length > 0));
+    };
+
+  const handleSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
+  useEffect(()=>{
+      const countryDataResult = filterDataTree(exampleJson, searchText);
+      setCountryData(countryDataResult);
+  }, [searchText]);
   return (
     <div>
+      <input type='text' name="search" value={searchText} onChange={handleSearchText} autoComplete='off'/>
+      <br/>
       <CountryNode data={countryData} />
     </div>
   );
